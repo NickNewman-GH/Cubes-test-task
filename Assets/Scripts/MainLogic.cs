edb111efb236504d475acab888b7e661dda06d11
Nikita <nitrix38r@gmail.com>
2025-12-30 02:15:53 +0800

    Movement

--- Assets/Scripts/MainLogic.cs
+++ Assets/Scripts/MainLogic.cs
@@ -16,7 +16,7 @@ public class MainLogic : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Vector2Int currentIndex = GetRandomIndex(dataManager.height, dataManager.width);
+        currentIndex = GetRandomIndex(dataManager.width, dataManager.height);
         cubesRenderer = new Renderer[cubes.Length];
         for (int i = 0; i < cubes.Length; i++)
             cubesRenderer[i] = cubes[i].GetComponent<Renderer>();
@@ -26,7 +26,23 @@ public class MainLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+           currentIndex.x = (currentIndex.x + 1 + dataManager.height) % dataManager.height;
+           Colorize(currentIndex);
+        } else if (Input.GetKeyDown(KeyCode.A))
+        {
+           currentIndex.y = (currentIndex.y - 1 + dataManager.width) % dataManager.width;
+           Colorize(currentIndex);
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+           currentIndex.x = (currentIndex.x - 1 + dataManager.height) % dataManager.height;
+           Colorize(currentIndex);
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+           currentIndex.y = (currentIndex.y + 1 + dataManager.width) % dataManager.width;
+           Colorize(currentIndex);
+        }
     }
 
     void Colorize(Vector2Int index)
@@ -41,7 +57,7 @@ public class MainLogic : MonoBehaviour
             }
     }
 
-    Vector2Int GetRandomIndex(int height, int width)
+    Vector2Int GetRandomIndex(int width, int height)
     {
         return new Vector2Int(Random.Range(0, width), Random.Range(0, height));
     }
